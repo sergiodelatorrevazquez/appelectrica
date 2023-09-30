206f0f43b0fc42ab4a9a4896609caae1fd356d3a
Sergiodltv <sergiodltv@uma.es>
2023-09-30 09:15:17 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-recomendaciones',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
